a17296c44b2973ddb23a12ccada8f6a70fcf9862
thegamershollow <thegamershollow@outlook.com>
2023-08-28 18:16:52 +0000

    added install script

---
+++ Mac OS App Setup/homebrew-install.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+xcode-select --install
+cd $HOME
+mkdir brew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C brew
+export PATH=$HOME/brew/bin:$PATH >> ~/.zshrc
+exec $SHELL
